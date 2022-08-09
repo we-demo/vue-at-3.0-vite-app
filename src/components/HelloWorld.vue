8939e7847e7b3055bd64026cba315927303c141d
Fritz Lin <uxfritz@163.com>
2022-08-09 20:20:43 +0800

    import and use vue-at #vue3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,27 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import VueAt from 'vue-at'
 
 defineProps({
   msg: String
 })
 
 const count = ref(0)
+
+let at = reactive({
+  members: [
+    'Bill Gates', 'Steve Jobs', '椿木', '小浩',
+  ],
+  text: 'xxx @椿木 @Bill Gates xxx...',
+})
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+
+  <VueAt :members="at.members" v-model:value="at.text">
+    <div class="editor" contenteditable=""></div>
+  </VueAt>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -36,5 +48,16 @@ const count = ref(0)
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+
+.editor {
+  white-space: pre-wrap;
+  overflow: hidden;
+  resize: none;
+  background-color: rgba(0, 255, 255, .02);
+  border: solid 1px gray;
+  padding: .4em 1em;
+  max-width: 30em;
+  height: 5em;
 }
 </style>
